7555e80fee22bd3a771e64233be5930ae8b27d79
Szymon Pankowski <57172847+spankowski@users.noreply.github.com>
2021-08-01 17:07:47 +0200

    commit

--- demo/models.py
+++ demo/models.py
@@ -16,3 +16,6 @@ class Book(models.Model):
     published = models.DateField(blank=True, null=True, default=None)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='covers/', blank=True)
+    
+    def __str__(self):
+        return self.title
--- demo/views.py
+++ demo/views.py
@@ -2,17 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
+from django.shortcuts import render
 
-
-class Another(View):
-
-    books = Book.objects.filter(is_published=True)
-    output = ''
-    for book in books:
-        output += f'We have {book.title} books with ID: {book.id} <br>'
-    def get(self, request):
-        return HttpResponse(self.output)
 
 # Create your views here.
 def first(request):
-    return HttpResponse('First message from demo')
+    books = Book.objects.all()
+    return render(request, 'first_temp.html',{'books':books})
+
+# Create your views here.
+def abc(request):
+    return HttpResponse('ABC message from demo')
